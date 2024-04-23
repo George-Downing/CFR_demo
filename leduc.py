424723adb7e20be3a8055a742a3f2704cd48db2a
George-Downing <72960511+George-Downing@users.noreply.github.com>
2024-04-23 22:26:15 +0800

    Add files via upload

--- leduc.py
+++ leduc.py
@@ -1,7 +1,11 @@
 import os
 import time
-import pickle
+# import pickle
+# from typing import Dict, Union, Any
+from matplotlib import pyplot as plt
+
 import numpy as np
+# from numpy import ndarray
 
 from extensive_form import player_t, act_t, Node, NodePtr, InfoSet, InfoSetPtr, rand_sig
 
@@ -27,6 +31,17 @@ class Leduc(object):
         for _info_title in many_round2_info_titles[_player_i]:
             for _c in calls_template:
                 many_round2_info_heads[_player_i].append(_info_title + "".join(_c))
+    added_money = [
+        (["r", "f"], np.array([1, 0], dtype=float)),
+        (["r", "r", "f"], np.array([1, 2], dtype=float)),
+        (["c", "r", "f"], np.array([0, 1], dtype=float)),
+        (["c", "r", "r", "f"], np.array([2, 1], dtype=float)),
+        (["r", "c"], np.array([1, 1], dtype=float)),
+        (["c", "c"], np.array([0, 0], dtype=float)),
+        (["r", "r", "c"], np.array([2, 2], dtype=float)),
+        (["c", "r", "c"], np.array([1, 1], dtype=float)),
+        (["c", "r", "r", "c"], np.array([2, 2], dtype=float))
+    ]
 
     def __init__(self):
         pass
@@ -38,11 +53,11 @@ def create_tree():
     true_root: NodePtr = Node(NodePtr(0), act_t("N/A")).p
     Node.roots.append(true_root)
     node_layers: list[list[NodePtr]] = [[true_root]]
-    info_collect: dict[player_t, dict[str, InfoSetPtr]] = {A: {}, B: {}, LUCK: {}}
-    act_map: dict[player_t, dict[InfoSetPtr, list[act_t]]] = {A: {}, B: {}, LUCK: {}}
+    info_collect: dict[player_t, dict[str, InfoSetPtr]] = {LUCK: {}, A: {}, B: {}}
+    act_map: dict[player_t, dict[InfoSetPtr, list[act_t]]] = {LUCK: {}, A: {}, B: {}}
 
     # 0->1->2->3: player="LUCK", (EQUIVALENT to a SINGLE composite step)
-    for n in node_layers[-1]:
+    for n in [true_root]:
         I = InfoSet(LUCK, "-".join(n.o.h())).p
         I.o.append(n)
         n.o.I = I
@@ -70,16 +85,17 @@ def create_tree():
     leaves: list[NodePtr] = []
     # round 1
     for l in range(len(Leduc.round_template)):
-        node_layers.append([])
-        for pa, acts in Leduc.round_template[l]:
-            acts = act_t.cast(acts)
-            player_i = A if l % 2 == 0 else B
-            for info_r in Leduc.many_round1_info_heads[player_i]:  # diff
+        player_i = A if l % 2 == 0 else B
+        for info_r in Leduc.many_round1_info_heads[player_i]:  # diff
+            for pa, acts in Leduc.round_template[l]:
+                acts = act_t.cast(acts)
                 obs = info_r + "".join(pa)
                 I = InfoSet(player_i, obs).p
                 info_collect[player_i][I.o.observation] = I
                 act_map[player_i][I] = acts
-            for r in many_round1_roots:  # diff
+        node_layers.append([])
+        for r in many_round1_roots:  # diff
+            for pa, acts in Leduc.round_template[l]:
                 n = r.o(act_t.cast(pa))
                 h = n.o.h()
                 obs = ((h[0][0] + "?" + "?") if (player_i == A) else ("?" + h[1][0] + "?")) + "".join(h[3:])  # diff
@@ -97,16 +113,17 @@ def create_tree():
             many_round2_roots.append(n)  # diff
     # round 2
     for l in range(len(Leduc.round_template)):
-        node_layers.append([])
-        for pa, acts in Leduc.round_template[l]:
-            acts = act_t.cast(acts)
-            player_i = A if l % 2 == 0 else B
-            for info_r in Leduc.many_round2_info_heads[player_i]:  # diff
+        player_i = A if l % 2 == 0 else B
+        for info_r in Leduc.many_round2_info_heads[player_i]:  # diff
+            for pa, acts in Leduc.round_template[l]:
+                acts = act_t.cast(acts)
                 obs = info_r + "".join(pa)
                 I = InfoSet(player_i, obs).p
                 info_collect[player_i][I.o.observation] = I
                 act_map[player_i][I] = acts
-            for r in many_round2_roots:  # diff
+        node_layers.append([])
+        for r in many_round2_roots:  # diff
+            for pa, acts in Leduc.round_template[l]:
                 n = r.o(act_t.cast(pa))
                 h = n.o.h()
                 obs = ((h[0][0] + "?" + h[2][0]) if (player_i == A) else ("?" + h[1][0] + h[2][0])) + "".join(h[3:])  # diff
@@ -127,8 +144,19 @@ def create_tree():
 
 
 if __name__ == "__main__":
+    TIME = {}
+    time_start = time.time()
+    TIME["start"] = time.time() - time_start
+
     A, B, LUCK = Leduc.A, Leduc.B, Leduc.LUCK
     true_root, node_layers, many_round1_roots, many_round2_roots, leaves, info_collect, act_map = create_tree()
+    # node_layers: [1, 6, 30, 120, 240, 600, 600, 240 (then 120 quited, and 480 joined), 1200, 3000, 3000, 1200]
+    # many_round1_roots: 120
+    # many_round2_roots: 600 = 120*5
+    # round1_leaves: 480 = 120*4
+    # round2_leaves: 5400 = 120*5*(4+5)
+    TIME["leduc"] = time.time() - time_start
+
     np.set_printoptions(precision=4, suppress=True)
     with open(os.path.join(PRINT_OUT, "node_layers.txt"), "w") as f:
         for l in node_layers:
@@ -144,14 +172,16 @@ if __name__ == "__main__":
         for n in leaves:
             print(n, file=f)
     with open(os.path.join(PRINT_OUT, "info_collect.txt"), "w") as f:
-        for player_i in [LUCK, A, B]:
+        for player_i in info_collect:
             for I in info_collect[player_i]:
                 print(I, ": ", info_collect[player_i][I], file=f, sep="")
     with open(os.path.join(PRINT_OUT, "act_map.txt"), "w") as f:
-        for player_i in [LUCK, A, B]:
+        for player_i in info_collect:
             for I in act_map[player_i]:
                 print(I, ": ", act_map[player_i][I], file=f, sep="")
+    TIME["print"] = time.time() - time_start
 
+    # sigma init
     sigma: dict[player_t, dict[InfoSetPtr, np.ndarray]] = {A: {}, B: {}, LUCK: {}}
     for obs in info_collect[LUCK]:
         I = info_collect[LUCK][obs]
@@ -166,13 +196,19 @@ if __name__ == "__main__":
         for player_i in [LUCK, A, B]:
             for I in sigma[player_i]:
                 print(I, ": ", sigma[player_i][I], file=f, sep="")
+    TIME["sigma"] = time.time() - time_start
 
-    pi: dict[NodePtr, float] = {true_root: 1.0}
+    # pi_init
+    pi: dict[NodePtr, np.ndarray] = {true_root: np.array(1.0)}
+    for l in node_layers:
+        for n in l:
+            pi[n] = np.array(1.0)
     for l in node_layers[0:3]:
         for n in l:
             act = act_map[LUCK][n.o.I]
             for i, a in enumerate(act):
                 pi[n.o.child[a]] = pi[n] * sigma[LUCK][n.o.I][i]
+    # pi_refresh
     for l in range(len(Leduc.round_template)):
         for pa, act in Leduc.round_template[l]:
             player_i = A if l % 2 == 0 else B
@@ -187,12 +223,86 @@ if __name__ == "__main__":
                 n = r.o(pa)
                 for i, a in enumerate(act):
                     pi[n.o.child[a]] = pi[n] * sigma[player_i][n.o.I][i]
+    # '''
     with open(os.path.join(PRINT_OUT, "pi.txt"), "w") as f:
         for l in node_layers:
             for n in l:
                 print(n, pi[n], file=f)
+    # '''
+    TIME["pi"] = time.time() - time_start
+
+    ante: dict[NodePtr, np.ndarray] = {}
+    for r in many_round1_roots:
+        ante[r] = np.array([1, 1], dtype=float)
+    for r in many_round1_roots:
+        for h, m in Leduc.added_money:
+            ante[r.o(h)] = ante[r] + 2 * m
+    for r in many_round2_roots:
+        for h, m in Leduc.added_money:
+            ante[r.o(h)] = ante[r] + 4 * m
 
     cfv: dict[NodePtr, np.ndarray] = {}
-    # for leaves:
+    for l in node_layers:
+        for n in l:
+            cfv[n] = np.array([0.0, 0.0], dtype=float)
+    # FOR LEAVES: init
+    # round-1, fold leaves
+    for r in many_round1_roots:
+        for seq in Leduc.folds_template:
+            leaf = r.o(seq)
+            if len(seq) % 2 == 1:
+                cfv[leaf] = np.array([-1, 1]) * ante[leaf][0] - ante[leaf] * 0.05
+            else:
+                cfv[leaf] = np.array([1, -1]) * ante[leaf][1] - ante[leaf] * 0.05
+    # round-2, fold leaves
+    for r in many_round2_roots:
+        for seq in Leduc.folds_template:
+            leaf = r.o(seq)
+            if len(seq) % 2 == 1:
+                cfv[leaf] = np.array([-1, 1]) * ante[leaf][0] - ante[leaf] * 0.05
+            else:
+                cfv[leaf] = np.array([1, -1]) * ante[leaf][1] - ante[leaf] * 0.05
+    # round-2, call leaves
+    for r in many_round2_roots:
+        h = r.o.h()
+        winner = "/"
+        if h[0][0] == h[1][0]:
+            winner = "/"  # the only way to tie, P=20%
+        elif h[0][0] == h[2][0]:
+            winner = "A"  # A win by match, P=20%
+        elif h[1][0] == h[2][0]:
+            winner = "B"  # B win by match, P=20%
+        else:  # mutually distinct
+            if h[0][0] == "K":
+                winner = "A"  # A win, P=13.33%
+            elif h[0][0] == "Q":
+                if h[1][0] == "J":
+                    winner = "A"  # A win, P=6.67%
+                elif h[1][0] == "K":
+                    winner = "B"  # B win, P=6.67%
+            elif h[0][0] == "J":
+                winner = "B"  # B win, P=13.33%
+        for h in Leduc.calls_template:
+            leaf = r.o(h)
+            if winner == "A":
+                cfv[leaf] = np.array([1, -1]) * ante[leaf][1] - ante[leaf] * 0.05
+            elif winner == "B":
+                cfv[leaf] = np.array([-1, 1]) * ante[leaf][0] - ante[leaf] * 0.05
+            elif winner == "/":
+                cfv[leaf] = -ante[leaf] * 0.05
+    # FOR OTHER NODES: refresh
 
-    # for other nodes:
+    # '''
+    with open(os.path.join(PRINT_OUT, "cfv.txt"), "w") as f:
+        for n in cfv:
+            print(n, cfv[n], file=f)
+    # '''
+    TIME["cfv"] = time.time() - time_start
+
+    plt.plot(TIME.values())
+    i = 0
+    for key in TIME.keys():
+        plt.text(i-0.5, TIME[key], key, horizontalalignment="center")
+        i += 1
+    # plt.show()
+
